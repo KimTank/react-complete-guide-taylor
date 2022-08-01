faa6fdecf7759edad38618ff9c1fe39c79891a34
taylor <sycork@gmail.com>
2022-08-01 20:52:29 +0900

    강의와 동기화

--- src/App.js
+++ src/App.js
@@ -30,7 +30,7 @@ const App = () => {
   return (
     <div className="App">
       <h2>Let's get started!</h2>
-      <Expenses expenses={expenses}/>
+      <Expenses items={expenses}/>
     </div>
   );
 }
