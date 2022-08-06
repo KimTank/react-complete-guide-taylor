c389071a009ccc6a38a73b2624906b03b51dc85b
taylor <sycork@gmail.com>
2022-08-06 16:38:46 +0900

    react 18 render방식 변경됨 -> index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,14 @@
-import  ReactDOM  from 'react-dom';
+import  {createRoot}  from 'react-dom/client';
 
 import './index.css';
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+/*
+ * https://ko.reactjs.org/blog/2022/03/08/react-18-upgrade-guide.html
+ * react 17 -> 18 deprecate -> ms explore는 지원중단되어 17사용해야됨
+ */
+//ReactDOM.render(<App />, document.getElementById('root'));
+const container = document.getElementById('root');
+const root = createRoot(container);
+root.render(<App tab="home" />);
